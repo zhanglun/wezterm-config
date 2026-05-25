f68d04bed15e4f4d332324c7651c111e4a7f65dc
zhanglun <zhanglun1410@gmail.com>
2026-05-25 17:51:02 +0800

    feat: update font and default shell
    - font: switch to Maple Mono NF, increase size to 14 on macOS
    - launch: set zsh as default shell, reorder launch menu

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,11 +1,11 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
--- local font_family = 'Maple Mono NF'
-local font_family = 'JetBrainsMono Nerd Font'
+local font_family = 'Maple Mono NF'
+-- local font_family = 'JetBrainsMono Nerd Font'
 -- local font_family = 'CartographCF Nerd Font'
 
-local font_size = platform.is_mac and 12 or 9.75
+local font_size = platform.is_mac and 14 or 9.75
 
 ---@type Config
 return {
--- config/launch.lua
+++ config/launch.lua
@@ -20,12 +20,12 @@ if platform.is_win then
       },
    }
 elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+   options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
+      { label = 'Zsh', args = { '/bin/zsh', '-l' } },
       { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
    options.default_prog = { 'fish', '-l' }
