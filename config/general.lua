eefd3afe540aaa4f412b2f1421272a9b7a950d5e
zhanglun <zhanglun1410@gmail.com>
2026-08-05 10:55:18 +0800

    fix(general): 将中断退出码视为正常退出

--- config/general.lua
+++ config/general.lua
@@ -3,6 +3,9 @@ return {
    -- behaviours
    automatically_reload_config = true,
    exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
+   -- zsh returns 130 when `exit` follows a command interrupted with Ctrl-C.
+   -- Treat it as clean so the pane closes instead of displaying a warning.
+   clean_exit_codes = { 130 },
    exit_behavior_messaging = 'Verbose',
    skip_close_confirmation_for_processes_named = {},
    status_update_interval = 1000,
